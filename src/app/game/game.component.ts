e6af08ce1a1eeb6e559fb5875bae7de157800587
Lempurdysh <ryuote@gmail.com>
2021-11-15 14:53:20 +0200

    null

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-game',
@@ -7,17 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class GameComponent implements OnInit {
+  ngOnInit(): void {
+  }
 
   constructor() {
     for (let i= 0; i <10; i++){
       this.arr15.sort(()=>Math.random() -0.5)
     }
+
     console.log(this.arr15);
   }
 
-  public arr15 = [1, 2, 3, 4, 5 ,6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  public arr15 = [0, 1, 2, 3, 4, 5 ,6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
-  ngOnInit(): void {
+  SwitchPosition(arg : any) : void {
+    this.arr15.sort(()=>Math.random() -0.5);
+    console.log(this.arr15)
   }
+
+
+
 
 }
