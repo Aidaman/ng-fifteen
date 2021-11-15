62da4216de9334f4807eaf4b5b362e86494010a0
Lempurdysh <ryuote@gmail.com>
2021-11-15 14:52:47 +0200

    finalMarkupAndSomeLogic

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -5,9 +5,17 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css']
 })
+
 export class GameComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    for (let i= 0; i <10; i++){
+      this.arr15.sort(()=>Math.random() -0.5)
+    }
+    console.log(this.arr15);
+  }
+
+  public arr15 = [1, 2, 3, 4, 5 ,6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
   ngOnInit(): void {
   }
